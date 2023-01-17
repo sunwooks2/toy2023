c60eb5842e313396779e7f589c56706a882ae1d7
COVISION\swkang <swkang@covision.co.kr>
2023-01-17 14:13:43 +0900

    spring boot & gradle 프로젝트 생성

---
+++ src/main/java/com/example/toy2023/Toy2023Application.java
@@ -0,0 +1,13 @@
+package com.example.toy2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toy2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toy2023/Toy2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toy2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

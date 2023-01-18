659a798274d66c97fb74abcb97e8065a7b111016
COVISION\swkang <swkang@covision.co.kr>
2023-01-18 17:45:40 +0900

    spring boot view resolver ok

--- src/main/java/com/example/toy2023/controller/ToyController.java
+++ src/main/java/com/example/toy2023/controller/ToyController.java
@@ -3,8 +3,9 @@ package com.example.toy2023.controller;
 import com.example.toy2023.service.ToyService;
 import com.example.toy2023.vo.ToyVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -14,12 +15,17 @@ public class ToyController {
     @Autowired
     private ToyService toyService;
 
-    @RequestMapping(value="/")
+    @RequestMapping(value="/getUser")
     public void main() {
         ArrayList<ToyVO> voList = toyService.getUserList();
         for(ToyVO vo : voList) {
             System.out.println(vo.getId());
             System.out.println(vo.getName());
         }
+    }
+    @GetMapping(value="/home")
+    public String hello (Model model) {
+        model.addAttribute("data", "hello!");
+        return "toyHome";
     }
 }
